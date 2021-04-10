80e2d51a169e8fcb3fcfe4fcebeb7d2609420eb2
ASHEESH KUMAR <asheeshkumar96@hotmail.com>
2021-04-10 23:16:07 +0530

    urlify, in place

--- src/ds/arraysAndStrings/URLify.java
+++ src/ds/arraysAndStrings/URLify.java
@@ -9,8 +9,37 @@ package ds.arraysAndStrings;
  * Output:"Mr%20John%20Smith", 13
  */
 public class URLify {
+	
+	// official
+	public static void replaceSpaces(char[] str, int trueLength) {
+		int spaceCount = 0, i = 0;
+		for(i = 0; i < trueLength; i++) {
+			if(str[i] == ' ') {
+				spaceCount++;
+			}
+		}
+		int index = trueLength + spaceCount * 2;
+		if(trueLength < str.length) {
+			str[trueLength] = '\0'; // End array
+		}
+		
+		for(i = trueLength - 1; i >= 0; i--) {
+			if(str[i] == ' ') {
+				str[index - 1] = '0';
+				str[index - 2] = '2';
+				str[index - 3] = '%';
+				index = index - 3;
+			} else {
+				str[index - 1] = str[i];
+				index--;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-		
+		char[] chars = "I am John    ".toCharArray();
+		System.out.println(new String(chars));
+		replaceSpaces(chars, 9);
+		System.out.println(new String(chars));
 	}
 }
