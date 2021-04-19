aa382c85a40a8f9ed5f449d7b8299225a9bf15e0
ASHEESH KUMAR <asheeshkumar96@hotmail.com>
2021-04-19 17:56:51 +0530

    string rotation is very tricky

--- src/ds/arraysAndStrings/StringRotation.java
+++ src/ds/arraysAndStrings/StringRotation.java
@@ -6,9 +6,23 @@ package ds.arraysAndStrings;
  * (e.g., "watterbottle" is a rotation of "erbottlewat").
  */
 public class StringRotation {
-
-	public static void main(String[] args) {
-		System.out.println("String Rotation");
+	
+	// also official
+	private static boolean isRotation(String s1, String s2) {
+		int len = s1.length(); 
+		/* check that s1 and s2 are equal length and not empty */
+		if(len == s2.length() && len > 0) {
+			String s1s1  = s1 + s1;
+			return isSubstring(s1s1, s2);
+		}
+		return false;
+	}
+	
+	private static boolean isSubstring(String s1, String s2) {
+		return s1.contains(s2);
 	}
 
+	public static void main(String[] args) {
+		System.out.println(isRotation("waterbottle", "erbottlewat"));
+	}
 }
