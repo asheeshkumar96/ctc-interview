bbcc36db21c2297afb9cf4af561b0e7c282d47e7
ASHEESH KUMAR <asheeshkumar96@hotmail.com>
2021-04-24 18:16:15 +0530

    the logical discription of loop detection is out of comprehend

--- src/ds/linkedLists/LoopDetection.java
+++ src/ds/linkedLists/LoopDetection.java
@@ -1,8 +1,40 @@
 package ds.linkedLists;
 
 public class LoopDetection {
+	
+	private static LinkedListNode findBeginning(LinkedListNode head) {
+		LinkedListNode slow = head; 
+		LinkedListNode fast = head;
+		
+		// find metting point, 
+		while(fast != null && fast.next != null) {
+			slow = slow.next; 
+			fast = fast.next.next;
+			if(slow == fast) { // collision
+				break;
+			}
+		}
+		
+		// error check - no meeting point, and therefore no loop
+		if(fast == null || fast.next == null) return null;
+		
+		slow = head;
+		while(slow != fast) {
+			slow = slow.next; 
+			fast = fast.next;
+		}
+		return fast;
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Loop detection");
+		LinkedListNode head = new LinkedListNode('A');
+		head.next = new LinkedListNode('B');
+		head.next.next = new LinkedListNode('C');
+		head.next.next.next = new LinkedListNode('D');
+		head.next.next.next.next = new LinkedListNode('E');
+		head.next.next.next.next.next = head.next.next;
+		
+		char result = (char) findBeginning(head).data;
+		System.out.println(result);
 	}
 }
