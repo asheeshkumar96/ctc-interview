d67340010672ff7ccdd310dacde894436cb0d472
ASHEESH KUMAR <asheeshkumar96@hotmail.com>
2021-04-25 19:00:44 +0530

    added skeleton for c plus plus specific topics

---
+++ src/xtras/cplusplus/LastKLines.java
@@ -0,0 +1,8 @@
+package xtras.cplusplus;
+
+public class LastKLines {
+
+	public static void main(String[] args) {
+		System.out.println("last k lines");
+	}
+}
---
+++ src/xtras/cplusplus/ReverseString.java
@@ -0,0 +1,8 @@
+package xtras.cplusplus;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		System.out.println("Reverse String");
+	}
+}
