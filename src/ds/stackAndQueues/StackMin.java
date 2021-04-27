642f02bbb6c5015df15bd15d4b00f00d00a0eafb
ASHEESH KUMAR <asheeshkumar96@hotmail.com>
2021-04-28 05:27:28 +0530

    stack with min() in O(1)

--- src/ds/stackAndQueues/StackMin.java
+++ src/ds/stackAndQueues/StackMin.java
@@ -3,6 +3,35 @@ package ds.stackAndQueues;
 public class StackMin {
 
 	public static void main(String[] args) {
-		System.out.println("StackMin");
+		StackWithMin stackWithMin = new StackWithMin();
+		System.out.println(stackWithMin.min());
+		stackWithMin.push(3);
+		System.out.println(stackWithMin.min());
+		
+		stackWithMin.push(2);
+		System.out.println(stackWithMin.min());
+		stackWithMin.push(0);
+		System.out.println(stackWithMin.min());
+		stackWithMin.push(-1);
+		System.out.println(stackWithMin.min());
+		stackWithMin.pop();
+		stackWithMin.pop();
+		System.out.println(stackWithMin.min());
+		
+		
+		System.out.println("-------------------------------------");
+		StackWithMin2 stackWithMin2 = new StackWithMin2();
+		System.out.println(stackWithMin2.min());
+		stackWithMin2.push(5);
+		System.out.println(stackWithMin2.min());
+		stackWithMin2.push(0);
+		System.out.println(stackWithMin2.min());
+		stackWithMin2.push(-1);
+		System.out.println(stackWithMin2.min());
+		stackWithMin2.pop();
+		System.out.println(stackWithMin2.min());
+		stackWithMin2.pop();
+		System.out.println(stackWithMin2.min());
+		
 	}
 }
